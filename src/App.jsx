06380aabc4970523f258647f1e3e8c0dc5759e01
code-with-prabhu <prabhudattarout2005@gmail.com>
2026-08-05 22:22:01 +0530

    Already done most of the work

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
+import Navbar from './components/Navigation/Navbar'
+import FullScreenNav from './components/Navigation/FullScreenNav'
 
 const App = () => {
   return (
     <>
+    <Navbar />
+    <FullScreenNav />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/projects' element={<Projects/>} />
--- src/components/home/HomeBottom.jsx
+++ src/components/home/HomeBottom.jsx
@@ -17,7 +17,7 @@ const HomeBottom = () => {
         </Link>
       </div>
       <div className="lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50]  lg:h-25 flex items-center px-3 pt-1 pb-2 lg:px-14 border-white rounded-full uppercase">
-        <Link className="text-[6vw] lg:mt-6" to="/agence">
+        <Link className="text-[6vw] lg:mt-6" to="/skills">
           Skills
         </Link>
       </div>
